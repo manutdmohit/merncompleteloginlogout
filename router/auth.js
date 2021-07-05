b00b942ee5ccb4af9b40f1e8bfb51a1a15cc7082
manutdmohit <saudmohit@gmail.com>
2021-07-05 10:58:24 +0545

    /

--- router/auth.js
+++ router/auth.js
@@ -8,6 +8,10 @@ const authenticate = require("../middleware/authenticate");
 require("../db/conn");
 const User = require("../model/userSchema");
 
+router.get("/", (req, res) => {
+  res.send("Hello from the server");
+});
+
 //using promises
 // router.post("/register"(req, res) => {
 //     const { name, email, phone, work, password, cpassword } = req.body
